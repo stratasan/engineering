6843140e2a4ba0f03ad7472d22a99b38ddb433e4
Scott Burns <scott.s.burns@gmail.com>
2016-10-28 10:01:53 -0500

    Use new bucket when uploading

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 mkdocs build --clean
-aws s3 sync ./site s3://stratasan-engineering-manual/ --acl public-read --cache-control "public, max-age=86400" 
+aws s3 sync ./site s3://engineering.stratasan.com/ --acl public-read --cache-control "public, max-age=86400" 
